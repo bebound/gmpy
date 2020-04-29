e8bbc5d97fdb067f7b502079eb7ad73d53f37958
KK <bebound@gmail.com>
2020-04-29 23:56:21 +0800

    Update before_ci_build.sh

--- before_ci_build.sh
+++ before_ci_build.sh
@@ -1,4 +1,2 @@
-export PATH="/usr/lib/ccache:$PATH"
-ccache -M 256M && ccache -s
-sudo yum install gmp-devel mpfr-devel mpc-devel
-pip install Cython --verbose --disable-pip-version-check
+yum install gmp-devel mpfr-devel mpc-devel
+pip install Cython
