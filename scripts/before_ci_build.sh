b9ac3201e23847c0a1b766b149b3ddaf11de122f
KK <bebound@gmail.com>
2020-04-30 20:43:31 +0800

    Update

--- scripts/before_ci_build.sh
+++ scripts/before_ci_build.sh
@@ -1,8 +1,10 @@
-yum install -y gmp-devel mpfr-devel
+yum install -y wget  lzip
+wget https://gmplib.org/download/gmp/gmp-6.2.0.tar.lz
+tar -xvf gmp-6.2.0.tar.lz
+cd gmp-6.2.0 && ./configure && make -j && make install && cd ../
+wget https://www.mpfr.org/mpfr-current/mpfr-4.0.2.tar.gz
+tar -xvf mpfr-4.0.2.tar.gz && cd mpfr-4.0.2 && ./configure && make -j && make install && cd ../
 wget https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz
 tar -xvf mpc-1.1.0.tar.gz
-cd mpc-1.1.0
-./configuration
-make
-make install
+cd mpc-1.1.0 && ./configure && make -j && make install && cd ../
 pip install Cython
